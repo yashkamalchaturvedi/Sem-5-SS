1e79c7222dffedb50a6cf2b707110fa201fe76ab
Yash Kamal Chaturvedi <69413160+yashkamalchaturvedi@users.noreply.github.com>
2021-07-11 12:39:37 +0530

    2nd commit

---
+++ add2numbers.sh
@@ -0,0 +1,12 @@
+echo "Enter first number"
+read a
+echo "Enter second number"
+read b
+sum=$(($a + $b))
+echo "Sum of $a and $b: "$sum
+
+# Use ‘echo’ command to print output.
+# Take two inputs and store it in two different variables.
+# We can use ‘echo’ or ‘expr’ to calculate arithmetic operation. 
+# But here we have used just dollar sign to perform operation.
+
