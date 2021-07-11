f19d4c174e50619e90e1b6aac123065cacd87db4
Yash Kamal Chaturvedi <69413160+yashkamalchaturvedi@users.noreply.github.com>
2021-07-11 12:36:21 +0530

    1st commit

---
+++ helloworld.sh
@@ -0,0 +1,5 @@
+echo Hello World
+
+# ‘echo’ command without any option then a newline is added by default. 
+# ‘-n’ option is used to print any text without new line.
+# ‘-e’ option is used to remove backslash characters from the output.
