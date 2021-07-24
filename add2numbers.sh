c4b4274d40b881680fd694f89c7f0e48a473da8c
Yash Kamal Chaturvedi <69413160+yashkamalchaturvedi@users.noreply.github.com>
2021-07-24 10:06:22 +0530

    13th commit

--- add2numbers.sh
+++ add2numbers.sh
@@ -9,4 +9,3 @@ echo "Sum of $a and $b: "$sum
 # Take two inputs and store it in two different variables.
 # We can use ‘echo’ or ‘expr’ to calculate arithmetic operation. 
 # But here we have used just dollar sign to perform operation.
-
